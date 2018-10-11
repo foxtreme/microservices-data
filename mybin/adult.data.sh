d83d8b2e04079159c880d79f7c0a65b758123ee2
josanabr <john.sanabria@gmail.com>
2018-10-11 19:31:53 +0000

    adicionando comentarios

--- mybin/adult.data.sh
+++ mybin/adult.data.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 PREFIXDIR="/usr/local/data"
+# Nos ubicamos en el directorio ${PREFIXDIR}
 cd ${PREFIXDIR}
+# Descargando archivo
 curl http://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data > ${PREFIXDIR}/adult.data
+# Este archivo hace las veces de semaforo, al crearse da luz verde
 echo "Creando archivo $(pwd)/terminado.txt"
 touch terminado.txt
--- mybin/rmquestionmark.sh
+++ mybin/rmquestionmark.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Se recibira el nombre del archivo a procesar como argumento de este script
 ARCHIVO="/usr/local/data/terminado.txt"
+# Si el archivo no ha sido creado, indica que descarga no ha terminado 
 while [ ! -f "${ARCHIVO}" ]; do
   sleep 2
 done
 echo "Eliminando signos de interrogacion"
 sed -i.bak "s/, ?,/,,/g" $1
+# Se elimina el archivo que se uso como semaforo
 rm "${ARCHIVO}"
